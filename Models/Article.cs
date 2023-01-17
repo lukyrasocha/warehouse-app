6a4c23b1b746a6dd33480963e0dce660a1545c26
lukyrasocha <rasocha.lukas@gmail.com>
2023-01-17 17:07:35 +0100

    Add Article model

---
+++ Models/Article.cs
@@ -0,0 +1,11 @@
+
+namespace warehouse_app.Models;
+
+public class Article
+{
+    public string? art_id {get; set;}
+
+    public string? name {get; set;}
+
+    public string? stock {get; set;}
+}
